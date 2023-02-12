6b97a6016e66bef46678e4327e5000c89538ca91
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-12 13:27:08 -0500

    Initial Commit

---
+++ HWswift1.13/HWswift1_13App.swift
@@ -0,0 +1,17 @@
+//
+//  HWswift1_13App.swift
+//  HWswift1.13
+//
+//  Created by Astrid Snäll on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HWswift1_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
